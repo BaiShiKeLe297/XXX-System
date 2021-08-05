858ef7230e2746b07bbb83dab2f5576adfbfae72
BaiShiKeLe297 <baishi_kele297@126.com>
2021-08-05 23:05:30 +0800

    测试一下git

--- src/test/java/com/zq/zhiqaing/service/impl/TeacherServiceTest.java
+++ src/test/java/com/zq/zhiqaing/service/impl/TeacherServiceTest.java
@@ -4,4 +4,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TeacherServiceTest {
 
+    public static void main(String[] args) {
+
+
+        System.out.println("测试一下git提交");
+
+    }
+
 }
