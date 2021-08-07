e68c0f816a8e0073bfefc36ba4932d61fea18b1f
百事可乐 <baishi_kele297@126.com>
2021-08-07 14:11:20 +0800

    更改git提交用户名

--- src/test/java/com/zq/zhiqaing/service/impl/TeacherServiceTest.java
+++ src/test/java/com/zq/zhiqaing/service/impl/TeacherServiceTest.java
@@ -9,6 +9,9 @@ class TeacherServiceTest {
 
         System.out.println("测试一下git提交");
 
+        System.out.println("试一下 变更 git 提交的名字");
+        System.out.println("就 C 盘-> 用户 -> .gitConfig 配置文件 ->更改名字即可 ");
+
     }
 
 }
